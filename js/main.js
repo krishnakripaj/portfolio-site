81b2fb3f41b4393de8dc0811319467b7b78ae50c
Kripa Jayakumar <kripa.jay@gmail.com>
2023-11-23 18:46:24 +0530

    Added resume and image click onto a popup

--- js/main.js
+++ js/main.js
@@ -4,3 +4,24 @@ collapsibles.forEach((item) =>
     this.classList.toggle("collapsible--expanded");
   })
 );
+
+// Gallery Modal functionality
+var modal = document.getElementById("gallery-modal");
+var modalImg = document.getElementById("modal-img");
+var captionText = document.getElementById("modal-caption");
+const galleryImages = document.querySelectorAll(`[id^="gimg-"]`);
+// console.log(galleryImages)
+galleryImages.forEach((item)=>{
+  item.addEventListener("click", function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+  })
+})
+
+var span = document.getElementsByClassName("close")[0];
+if (span){
+span.onclick = function() {
+  modal.style.display = "none";
+} 
+}
